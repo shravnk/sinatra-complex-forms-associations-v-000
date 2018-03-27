97c2899923c23ed2bb3efaae586df2b1a91da126
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-27 22:44:53 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,6 +38,7 @@ class PetsController < ApplicationController
     if !params['owner']['name'].empty?
       @pet.owner << Pet.create(name: params['owner']['name'])
     end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
